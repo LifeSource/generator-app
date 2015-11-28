bf336ce6896f7b7fa1f89e8ad648724bba22fe61
Kenrick Wu <kenrick.wu@gmail.com>
2015-11-28 17:57:22 +0800

    Adding the .gitignore file to generator.

--- generators/app/index.js
+++ generators/app/index.js
@@ -98,6 +98,7 @@ module.exports = generators.Base.extend({
             { name: "settings/_.bowerrc", path: "./.bowerrc" },
             { name: "settings/_.jshintrc", path: "./.jshintrc" },
             { name: "settings/_.editorconfig", path: "./.editorconfig" },
+            { name: "settings/_.gitignore", path: "./.gitignore" },
             { name: "settings/_.README.md", path: "./README.md" },
             { name: "js/_gulpfile.js", path: "./gulpfile.js"},
             { name: "js/_karmaconf.js", path: "./karmaconf.js"},
