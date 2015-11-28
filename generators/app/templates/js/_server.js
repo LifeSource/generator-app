4dd9fcc22f2e65f52fa2f900d3a5cc6aad182704
Kenrick Wu <kenrick.wu@gmail.com>
2015-11-28 12:03:02 +0800

    Fixed a spelling mistake in package.json for the files section.

--- generators/app/templates/js/_server.js
+++ generators/app/templates/js/_server.js
@@ -6,9 +6,16 @@ var config = require("../../config")();
 
 var app = express();
 
+// database setup
+// mongoose.connect("mongodb://localhost/<appName>")
+
+// middleware setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// api goes here.
+
+// static files
 app.use(express.static(config.client));
 app.use(express.static(config.root));
 app.use("/*", express.static(config.index));
